e3328814e31e83453abf5e8270b39d44392f67d4
Dorato <cadorato@email.neit.edu>
2023-10-17 09:20:16 -0400

    Adding exercise 3.2

---
+++ Exercises/Week 3/Week3b.js
@@ -0,0 +1,18 @@
+console.log("Week3b running");
+
+var ammo = 100;
+console.log(ammo);
+console.log("Ammo = " + ammo);
+
+ammo = prompt("How much ammo?");
+console.log("Ammo is now = " + ammo);
+
+var a = 0;
+var b = 0;
+var sum = 0;
+a = prompt("Enter a value: ");
+a = Number(a);
+b = prompt("Enter another value: ");
+b = Number(b);
+sum = a + b;
+console.log("The sum of a and b is " + sum);
