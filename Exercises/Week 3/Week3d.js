f3e6e98133f2842e69423de090cd633f13840a52
Dorato <cadorato@email.neit.edu>
2023-10-17 10:57:08 -0400

    Updating test & adding exercise 3.3

---
+++ Exercises/Week 3/Week3d.js
@@ -0,0 +1,21 @@
+console.log("Week3d running");
+
+console.log("<<< MAIN MENU >>>");
+console.log("                 ");
+console.log("<1> Instructions ");
+console.log("<2> Play Game    ");
+console.log("<3> Quit         ");
+console.log("                 ")
+console.log("Select a number: ");
+
+var a = Number(prompt("Enter any value: "));
+var b = Number(prompt("Now enter another: "));
+var difference = 0;
+
+if (a>b) {
+    difference = a - b;
+} else {
+    difference = b - a;
+}
+
+console.log("The difference between these two values is " + difference);
---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Testing");
+
+var b = document.querySelector('button');
+
+b.addEventListener('click', function() {
+        var vergil = prompt("PUT A NUMBER");
+        console.log(vergil);
+});
