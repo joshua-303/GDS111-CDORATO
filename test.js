69f0e32142dd5d0b66637d94a79b5fb61ceeed5d
Dorato <cadorato@email.neit.edu>
2023-10-24 09:19:10 -0400

    Cool test update

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ console.log("Testing");
 var b = document.querySelector('button');
 
 b.addEventListener('click', function() {
-        var vergil = prompt("PUT A NUMBER");
-        console.log(vergil);
+       /* var vergil = prompt("PUT A NUMBER");
+        console.log(vergil); */
+        window.location.href = 'https://www.devilmaycry.com/en/';
 });
