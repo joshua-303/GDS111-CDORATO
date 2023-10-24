d020ba59efd908bce5282ce44dca82b71e965736
Dorato <cadorato@email.neit.edu>
2023-10-24 09:54:16 -0400

    Test update + more week 4 assignments

---
+++ Exercises/Week 4/Week4b.js
@@ -0,0 +1,17 @@
+let age = 18;
+
+age = age + 1;
+console.log("You are now " + age + " years old.");
+
+age++;
+console.log("You are now " + age + " years old.");
+
+let ammo = 6;
+ammo  = ammo - 1;
+ammo--;
+console.log("Ammo left in chamber: " + ammo);
+
+let balance = 0;
+balance = balance + 4.18;
+balance-=0.50;
+console.log("You pay: " + balance);
--- test.js
+++ test.js
@@ -5,5 +5,9 @@ var b = document.querySelector('button');
 b.addEventListener('click', function() {
        /* var vergil = prompt("PUT A NUMBER");
         console.log(vergil); */
-        window.location.href = 'https://www.devilmaycry.com/en/';
+        let yn = confirm("THIS IS SENDING YOU TO DEVIL MAY CRY.\nDO YOU ACCEPT?");
+
+        if (yn === true) {
+                window.location.href = 'https://www.devilmaycry.com/en/';
+        }
 });
