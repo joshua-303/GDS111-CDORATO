8c7bcbbf394a378bca4ba84bb4b54840d807bc9a
Dorato <cadorato@email.neit.edu>
2023-10-26 10:55:33 -0400

    Committing 4.4

---
+++ Exercises/Week 4/Week4d.js
@@ -0,0 +1,22 @@
+let fName = "Larry";
+let age = 22;
+let salary = 40000.00;
+let employed = true;
+
+console.log(fName + " is " + age + " years old.");
+console.log("He is making " + salary + " annually.");
+console.log(typeof fName);
+console.log(typeof age);
+console.log(typeof salary);
+console.log(typeof employed);
+
+fName = 77;
+console.log("fName = " + fName);
+console.log(typeof fName);
+
+const player = ["Jeff", "Joshua", "Omar", "John"];
+console.log("Their name is " + player[0]);
+
+for (let name = 0; name < 4; name++) {
+    console.log("Their name is " + player[name]);
+}
