3320456bcf5f9f680f6205168d1c43d674661dca
Dorato <cadorato@email.neit.edu>
2023-11-02 10:42:24 -0400

    Committing exercise 5.1

---
+++ Exercises/Week 5/Week5a.js
@@ -0,0 +1,8 @@
+let x = Math.floor(Math.random() * 10) + 1;
+
+console.log("The number is " + x);
+if (x == 7||x == 11) {
+    console.log("Congratulations! You win!");
+} else {
+    console.log("Too bad, you lose.");
+}
