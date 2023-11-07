7c054a8935211654d8d0b6590de83292435c98b1
unknown <cadorato@email.neit.edu>
2023-11-07 10:29:32 -0500

    Silly style

---
+++ Exercises/Week 5/function.js
@@ -0,0 +1,5 @@
+function func(p1, p2) { //calls the function command with the name "func" with the parameters "p1 and p2"
+    return p1 * p2; //executes this block of code when the function is called
+}
+
+console.log(func(3, 4));
--- Exercises/Week 5/go fish.js
+++ Exercises/Week 5/go fish.js
@@ -1,14 +1,17 @@
+function randCard() {
+ return Math.floor(1 + (14-1) * Math.random());   
+}
 let card  = [];
 
 console.log("Your opening hand: ");
 for(let num = 0; num < 7; num++) {
-    card.push(Math.floor(1 + (14-1) * Math.random()));
+    card.push(randCard());
     console.log(card[num]);
 }
 console.log("Press enter to draw another card!");
 
 function draw() {
-    card.push(Math.floor(1 + (14-1) * Math.random()));
+    card.push(randCard());
     console.log("You drew " + card[card.length - 1]);
 }
 
