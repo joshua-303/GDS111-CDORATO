e2c255cabef56907e6ae593e22484ed2b43989e1
Dorato <cadorato@email.neit.edu>
2023-11-09 10:51:47 -0500

    Exercise 6.3

---
+++ Exercises/Week 6/Week6b.js
@@ -0,0 +1,24 @@
+myFunction();
+func2(7);
+let twiceNum = doubleIt(5);
+let msg = message();
+console.log(msg);
+console.log("Function doubleIt() returns = " + twice);
+
+function myFunction() {
+    console.log("I'm inside myFunction()");
+}
+
+function func2(p1) {
+    console.log(p1);
+    console.log("Inside func2");
+}
+
+function doubleIt(num1) {
+    let d = num1 * 2;
+    return d;
+}
+
+function message() {
+    return "This is a message";
+}
