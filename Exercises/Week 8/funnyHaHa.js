a4c15b4660e01a2df38ac1dbb607bc65ad2ed786
Dorato <cadorato@email.neit.edu>
2023-12-05 08:17:06 -0500

    Exercise 9.1

---
+++ Exercises/Week 8/funnyHaHa.js
@@ -0,0 +1,3 @@
+let gameState = 'start';
+gameState = gameState == 'start' ? 'play' : 'start';
+console.log(gameState);
---
+++ Exercises/Week 9/week9a.js
@@ -0,0 +1,14 @@
+let start = Number(prompt("Enter a starting number:"));
+let end = Number(prompt("Enter an ending number:"));
+
+for(i=start; i<end; i++){
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz");
+    } else if(i % 3 == 0){
+        console.log("Fizz");
+    } else if(i % 5 == 0){
+        console.log("Buzz")
+    } else {
+        console.log(i);
+    }
+}
