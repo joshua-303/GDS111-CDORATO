86919095a2a180d6f71a1e82de15561861086b87
unknown <cadorato@email.neit.edu>
2023-12-12 08:45:01 -0500

    Final update

--- Exercises/final exam/final.js
+++ Exercises/final exam/final.js
@@ -6,16 +6,21 @@ for (pGuesses = 0; pGuesses < 10; pGuesses++) {
         break;
     }
     let pInput = prompt("Guess a number between 1 and 100! You've got 5 attempts!");
+    while (pInput < 1 || pInput > 100 || isNaN(pInput)) {
+        //cancels operation if user hits esc
+        if (pInput == null) {
+            break;
+        }
+        pInput = prompt("That's not a valid entry... Guess a number between 1 and 100!");
+    }
+    //cancels operation if user hits esc
     if (pInput == null) {
         break;
-    }
-    while (pInput < 1 || pInput > 100 || isNaN(pInput)) {
-        pInput = prompt("That's not a valid entry... Guess a number between 1 and 100!");
     }
     Number(pInput);
     console.log(pInput);
     if (pInput == secret) {
-        console.log("Congratulations, you won! The number was " + secret + "! You guessed correctly in " + pGuesses + " tries!");
+        console.log("Congratulations, you won! The number was " + secret + "! You guessed correctly in " + (pGuesses + 1) + " tries!");
         break;
     } else if (pInput > secret) {
         console.log("Your guess is too high, try again.");
